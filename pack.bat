3b55d62c6e161ff6ae3ffb2e60b63aa8fb0a5abc
Justin Fyfe <justin@fyfesoftware.ca>
2019-12-25 10:26:30 +0630

    Updated to use pakman

--- pack.bat
+++ pack.bat
@@ -4,19 +4,12 @@
 	FOR /D %%G IN (.\*) DO (
 		PUSHD %%G
 		IF EXIST "manifest.xml" (
-			sdb-pakr --compile --optimize --source=.\ --output=..\bin\org.santedb.sg.pak --keyFile=..\..\keys\community.santesuite.net.pfx --keyPassword=..\..\keys\community.santesuite.net.pass --embedcert
+			pakman --compile --optimize --source=.\ --output=..\bin\org.santedb.sg.pak --keyFile=..\..\keys\community.santesuite.net.pfx --keyPassword=..\..\keys\community.santesuite.net.pass --embedcert --install
 		)
 		POPD
 	)
 
-PUSHD bin
-
-copy ..\..\applets\bin\org.santedb.core.pak /y
-copy ..\..\applets\bin\org.santedb.bicore.pak /y
-copy ..\..\applets\bin\org.santedb.uicore.pak /y
-copy ..\..\applets\bin\org.santedb.config.pak /y
-copy ..\..\applets\bin\org.santedb.admin.pak /y
-copy ..\..\applets\bin\org.santedb.i18n.en.pak /y
-sdb-pakr --compose --source=..\santeguard.sln.xml -o santeguard.sln.pak --keyFile=..\..\keys\community.santesuite.net.pfx --embedCert --keyPassword=..\..\keys\community.santesuite.net.pass --embedcert
+mkdir dist
+pakman --compose --source=.\santeguard.sln.xml -o dist\santeguard.sln.pak --keyFile=..\keys\community.santesuite.net.pfx --embedCert --keyPassword=..\keys\community.santesuite.net.pass --embedcert
 POPD
 
